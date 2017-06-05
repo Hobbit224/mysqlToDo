f8e9a72e9ce30c0d842416cc77cc19a2470e19d9
Ian Booton <ianbooton@Ians-MacBook-Pro.local>
2017-06-05 14:46:56 -0400

    about to set up database

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+
+var config = require('../config/config');
+
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+	host: config.host,
+	user: config.userName,
+	password: config.password,
+	database: config.database
+})
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/addItem', function(req, res) {
+  // res.json(req.body)
+  var newTask = req.body.newTask
+  var DueDate = req.body.newTaskDate
 });
 
 module.exports = router;
