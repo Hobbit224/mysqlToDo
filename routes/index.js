663c3328ce9bb04e15ba288127c847dfbf1aac46
Ian Booton <ianbooton@Ians-MacBook-Pro.local>
2017-06-05 15:44:04 -0400

    hooked up to database

--- routes/index.js
+++ routes/index.js
@@ -12,15 +12,35 @@ var connection = mysql.createConnection({
 	database: config.database
 })
 
+connection.connect();
+// Now we can write some awesome queries
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	var message = req.query.msg;
+	if (message == 'added'){
+		message = "Your task was added!"
+	}
+	var selectQuery = "SELECT * FROM tasks;";
+	connection.query(selectQuery, function(error,results){
+		res.render('index', { 
+			message: message,
+			taskArray: results
+		});
+	})
+  
 });
 
 router.post('/addItem', function(req, res) {
   // res.json(req.body)
   var newTask = req.body.newTask
   var DueDate = req.body.newTaskDate
+  var insertQuery = "INSERT INTO tasks (taskName, taskDate) VALUES ('"+newTask+"','"+DueDate+"');"
+  // res.send(insertQuery);
+  connection.query(insertQuery, function(error, results){
+  	if(error) throw error;
+  	res.redirect('/?msg=added')
+  });
 });
 
 module.exports = router;
